7c2c167ba445356848f044582710ad56096ca169
Jeremiah Egwa <jerriee232@gmail.com>
2025-03-11 21:28:16 +0100

    completed the footer and stylings

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,9 @@
-
+// selecting elements
 const year = document.querySelector("#currentyear");
+const lastmodified = document.querySelector("#lastModified");
 const date = new Date()
 
+// Adding text content to elements dynamically
 year.innerHTML = date.getFullYear()
+
+lastmodified.innerHTML =`Last Modified: ${document.lastModified}`;
