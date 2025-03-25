a258b159079db4f33e4b188b17b82323c5207e72
Jeremiah Egwa <jerriee232@gmail.com>
2025-03-25 06:17:31 +0100

    finished activity: Handling Dom Events

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,23 +1,35 @@
 //variables
 const input = document.querySelector("#favchap");
-const button = document.querySelector("button");
 const list = document.querySelector("#list");
+const button = document.querySelector("button");
 
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
 
-//populate li with input value
-li.textContent = input.ariaValueMax;
-//populate delete button with ❌
-deleteButton.textContent = "❌";
-deleteButton.setAttribute("aria-label", "Remove Alma5");
+//Add event listener to button
+button.addEventListener("click", function(){
+    if (input.value.trim() !== '') { 
+        const li = document.createElement("li");
+        //populate li with input value
+        li.textContent = input.value;
+        const deleteButton = document.createElement("button");
+        //populate delete button with ❌
+        deleteButton.textContent = "❌";
+        deleteButton.setAttribute("aria-label", "Remove Alma5");
+        //append ❌ to the li
+        li.append(deleteButton);
+        //append li element to the unordered list element
+        list.appendChild(li);
 
-//append ❌ to the li
-li.append(deleteButton);
-//append li element to the unordered list element
-list.appendChild(li);
+        //Add event listener to button
+        deleteButton.addEventListener("click", function(){
+        list.removeChild(li);
+        input.focus();
+        })
+        input.value = "";
+        input.focus()
+    } else {
+        alert("Pleas enter a book and chapter");
+        input.focus()
+    }   
+})
 
-// console.log(input);
-// console.log(button);
-// console.log(deleteButton);
-// console.log(li);
+
